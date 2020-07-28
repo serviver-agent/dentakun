dc605532119cd733a8342de7b0479f053fb6f8f8
pxfnc <dev.pxfnc@gmail.com>
2020-07-29 01:35:02 +0900

    人様に優しいメッセージ追加

--- main.rb
+++ main.rb
@@ -4,6 +4,8 @@ calc = Calculator.new
 
 loop do
   begin
+    puts '式を入力してください。'
+    print '> '
     ipt = gets
     if ipt == "exit\n"
       puts 'bye!👎'
@@ -11,12 +13,12 @@ loop do
     end
     ipt.split.each do |token|
       if token.to_i.to_s == token
-        puts calc.push_number(token.to_i).result
+        puts "途中経過: #{calc.push_number(token.to_i).result}"
       else
         calc.push_op(token.to_sym)
       end
     end
-    puts "計算結果: #{calc.result}"
+    puts "\n計算結果: #{calc.result}\n\n"
   rescue StandardError => e
     puts e
   end
